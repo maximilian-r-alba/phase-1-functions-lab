8532143809ea156adbc5d62ac3d86e7c6b0ca3f7
Maximilian Alba <albam476@gmail.com>
2022-03-15 17:14:33 -0400

    commit tree aab5545d14cc03b0ffda44c693ab182d0177c545

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupSpot){
+    if (pickupSpot > 42){
+        return pickupSpot-42;
+    }
+    else{
+        return 42 - pickupSpot;
+    }
+}
+
+function distanceFromHqInFeet (pickupSpot){
+    return 264 * distanceFromHqInBlocks(pickupSpot);
+
+}
+
+function distanceTravelledInFeet(start, destination){
+    return destination > start ? ((destination - start)*264) : ((start - destination)*264);
+}
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start,destination) <= 400) {
+        return 0;
+    }
+    
+    else if (distanceTravelledInFeet(start,destination) >= 2500){
+        return 'cannot travel that far';
+    }
+
+    else if (distanceTravelledInFeet(start,destination) <= 2000){
+        return .02 *(distanceTravelledInFeet(start,destination) - 400);
+    }
+    else {
+        return 25;
+    }
+}
